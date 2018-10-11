b6460fc4cac905c9cd9843c77668649e1c770b5e
Jan <jansch2803@googlemail.com>
2018-10-11 21:35:33 +0200

    Decoder

---
+++ WindowsApplication1/Decoder.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Decoder
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.InputBox = New System.Windows.Forms.TextBox()
+        Me.OutputBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SeedBox = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'InputBox
+        '
+        Me.InputBox.Location = New System.Drawing.Point(12, 30)
+        Me.InputBox.MaxLength = 1000000
+        Me.InputBox.Multiline = True
+        Me.InputBox.Name = "InputBox"
+        Me.InputBox.Size = New System.Drawing.Size(525, 100)
+        Me.InputBox.TabIndex = 0
+        '
+        'OutputBox
+        '
+        Me.OutputBox.Location = New System.Drawing.Point(12, 204)
+        Me.OutputBox.Multiline = True
+        Me.OutputBox.Name = "OutputBox"
+        Me.OutputBox.ReadOnly = True
+        Me.OutputBox.Size = New System.Drawing.Size(525, 100)
+        Me.OutputBox.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 136)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 49)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Verschlüsseln"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(111, 136)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 49)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Entschlüsseln"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Input"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 188)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Output"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(210, 136)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(93, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Copy"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'SeedBox
+        '
+        Me.SeedBox.Location = New System.Drawing.Point(408, 166)
+        Me.SeedBox.MaxLength = 9
+        Me.SeedBox.Name = "SeedBox"
+        Me.SeedBox.Size = New System.Drawing.Size(129, 20)
+        Me.SeedBox.TabIndex = 7
+        Me.SeedBox.Text = "123456789"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(309, 163)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(93, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Reset Seed"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(210, 162)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(93, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Paste"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Decoder
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(549, 314)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.SeedBox)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.OutputBox)
+        Me.Controls.Add(Me.InputBox)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(565, 353)
+        Me.MinimumSize = New System.Drawing.Size(565, 39)
+        Me.Name = "Decoder"
+        Me.ShowIcon = False
+        Me.Text = "Decoder"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents InputBox As TextBox
+    Friend WithEvents OutputBox As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SeedBox As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ WindowsApplication1/Decoder.vb
@@ -0,0 +1,199 @@
+﻿Public Class Decoder
+    Dim InText As String
+    Dim OutText As String
+    Dim Letters(52) As String
+    Dim TextArray() As Char
+    Dim Seed() As Char
+    Dim SeedINT(8) As Integer
+    Dim Z As Integer = 0
+    Public Sub Main1()
+
+        Letters(0) = "a"
+        Letters(1) = "b"
+        Letters(2) = "c"
+        Letters(3) = "d"
+        Letters(4) = "e"
+        Letters(5) = "f"
+        Letters(6) = "g"
+        Letters(7) = "h"
+        Letters(8) = "i"
+        Letters(9) = "j"
+        Letters(10) = "k"
+        Letters(11) = "l"
+        Letters(12) = "m"
+        Letters(13) = "n"
+        Letters(14) = "o"
+        Letters(15) = "p"
+        Letters(16) = "q"
+        Letters(17) = "r"
+        Letters(18) = "s"
+        Letters(19) = "t"
+        Letters(20) = "u"
+        Letters(21) = "v"
+        Letters(22) = "w"
+        Letters(23) = "x"
+        Letters(24) = "y"
+        Letters(25) = "z"
+        Letters(26) = " "
+        Letters(27) = "("
+        Letters(28) = ")"
+        Letters(29) = "["
+        Letters(30) = "]"
+        Letters(31) = "{"
+        Letters(32) = "}"
+        Letters(33) = "?"
+        Letters(34) = "!"
+        Letters(35) = "="
+        Letters(36) = "'"
+        Letters(37) = ","
+        Letters(38) = "."
+        Letters(39) = "-"
+        Letters(40) = "/"
+        Letters(41) = "\"
+        Letters(42) = ":"
+
+        Letters(43) = "0"
+        Letters(44) = "1"
+        Letters(45) = "2"
+        Letters(46) = "3"
+        Letters(47) = "4"
+        Letters(48) = "5"
+        Letters(49) = "6"
+        Letters(50) = "7"
+        Letters(51) = "8"
+        Letters(52) = "9"
+
+
+
+
+        SeedChange()
+    End Sub
+
+
+
+
+
+
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Main1()
+        InText = InputBox.Text.ToLower
+        Z = 0
+        TextArray = InText.ToCharArray
+        Dim y As Integer = 0
+        Dim b As Integer = 0
+
+        For Each cha As Char In TextArray
+            Dim i As Integer = 0
+
+            For Each letter As String In Letters
+                If letter = cha Then
+
+
+
+                    If i + SeedINT(Z) <= Letters.Length - 1 Then
+                            TextArray(y) = Letters(i + SeedINT(Z))
+                        End If
+                        If i + SeedINT(Z) > Letters.Length - 1 Then
+                            Dim temp As Integer = (i + SeedINT(Z) - (Letters.Length))
+
+                            TextArray(y) = Letters(0 + temp)
+                        End If
+                        If Z = 8 Then
+                            Z = 0
+                        End If
+                        If Z <= 7 Then
+                            Z += 1
+                        End If
+
+                    End If
+
+
+                    i += 1
+            Next
+            y += 1
+
+        Next
+        OutputBox.Text = TextArray
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Main1()
+        InText = InputBox.Text
+        InText.ToLower()
+        TextArray = InText.ToCharArray
+        Z = 0
+        Dim y As Integer = 0
+
+        For Each cha As Char In TextArray
+            Dim i As Integer = 0
+
+            For Each letter As String In Letters
+                If letter = cha Then
+
+                    If i - SeedINT(Z) >= 0 Then
+                        TextArray(y) = Letters(i - SeedINT(Z))
+                    End If
+                    If i - SeedINT(Z) < 0 Then
+                        Dim temp As Integer = (i - SeedINT(Z) + 1)
+                        TextArray(y) = Letters((Letters.Length - 1) + temp)
+                    End If
+                    If Z = 8 Then
+                        Z = 0
+                    End If
+                    If Z <= 7 Then
+                        Z += 1
+                    End If
+
+                End If
+
+                i += 1
+
+
+            Next
+            y += 1
+        Next
+        OutputBox.Text = TextArray
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        My.Computer.Clipboard.SetText(OutputBox.Text)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        SeedBox.Text = "123456789"
+        SeedChange()
+    End Sub
+    Sub SeedChange()
+        Array.Clear(SeedINT, 0, SeedINT.Length)
+
+
+        Seed = SeedBox.Text.ToCharArray
+        Dim i As Integer = 0
+        For Each x As Char In Seed
+
+
+            SeedINT(i) = Asc(x) - 48
+
+            i += 1
+        Next
+        Z = 0
+    End Sub
+
+    Private Sub SeedBox_TextChanged(sender As Object, e As EventArgs) Handles SeedBox.TextChanged
+        SeedChange()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        InputBox.Text = My.Computer.Clipboard.GetText
+    End Sub
+
+End Class
